9065e16181c46d6badf7f58e4f4a6c98eec82c37
Caio <caioplay98@gmail.com>
2021-10-28 12:59:15 -0300

    Tests implementation

---
+++ tests/test_get_image.py
@@ -0,0 +1,24 @@
+import unittest
+import os
+from PIL import Image
+
+from src import get_image
+
+
+class GetImageTest(unittest.TestCase):
+
+    def test_image(self):
+        self.image = get_image.image(
+            lat=29.78, lon=-95.33, date='2018-01-01',
+            dim=0.15, api_key='DEMO_KEY'
+        )
+        self.image_test = Image.open(
+            os.path.join('test_images', 'testimage1.png')
+        )
+        self.assertEqual(
+            list(self.image.getdata()), list(self.image_test.getdata())
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
